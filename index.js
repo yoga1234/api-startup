49d1f6b40304501b2bc8b49156aa3201d4f9acd9
Septiana Yoga <septianayoga30@gmail.com>
2021-09-03 16:13:18 +0700

    adding base route

--- index.js
+++ index.js
@@ -2,3 +2,14 @@ const express = require("express");
 const request = require("request-promise");
 
 const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Amazong Scraper API");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
